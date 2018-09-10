99a370f494ecaa472599b6fd4bce46e17f22e6e2
Kanitin <60050142@kmitl.ac.th>
2018-09-10 18:47:20 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Adder{
+    
+	 public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+            System.out.printf("input number = "); a = sc.nextInt();
+            System.out.printf("input number = "); b = sc.nextInt();
+                c = a+b;    
+            System.out.println("Sum = "+c); 
+		
+    }
+}
